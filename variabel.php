38d06d6c86b26f1cd12359d08232bc4865f457f4
FranciskaWulandari <55430961+FranciskaWulandari@users.noreply.github.com>
2019-10-14 20:29:36 +0700

    first commit

---
+++ variabel.php
@@ -0,0 +1,45 @@
+   
+
+<!-- pembuat kode php -->
+
+<?php 
+
+// penulisan variable dan deklarasi variabel
+
+$nama = "andi";//deklarasi variabel nama
+
+$umur = "b17";//deklarasi variabel umur
+
+$a='Saya Sedang belajar PHP';//deklarasi variabel a
+
+$b=5;//deklarasi variabel b
+
+// menampilkan hasil dari variabel yang di deklarasikan
+
+echo $nama; //perintah outpun menampilkan isi dari nama
+
+echo "<br>"; // perintah output menambahkan spasi
+
+echo $umur;//perintah outpun menampilkan isi dari nama
+
+echo "<br>"; // perintah output menambahkan spasi
+
+
+
+print $a; //merupakan perintah output 
+
+echo $b;  //merupakan perintah output 
+
+// deklarsi variable $andi
+
+$andi="Andi";
+
+echo $Andi; // Notice: Undefined variable: Andi
+
+// error karena karakter pada variable yang di deklarasikan adalah a kecil bukan A besar
+
+
+
+?>
+
+<!-- akhiran dari sebuah script php -->
